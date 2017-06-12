b6b565925c88b4251fdd3033e610e0c224636033
h-github <hmohammadi@hotmail.ca>
2017-06-12 02:50:07 -0700

    Quote Entity

--- MyQuotesAPI/Entities/Quote.cs
+++ MyQuotesAPI/Entities/Quote.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MyQuotesAPI.Models
+namespace MyQuotesAPI.Entities
 {
-  public class QuoteDTO
-    {
+  public class Quote
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string UserId { get; set; }
     public string Content { get; set; }
